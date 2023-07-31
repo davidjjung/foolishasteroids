b0a1b9c419aaf34b240be974323afd8810a63915
DavigJ <david.j.jung@gmail.com>
2023-07-30 23:00:10 -0400

    elixirs based solely on Pehkui commands are now entirely within scope (whoa)

--- src/main/java/com/davigj/foolish_asteroids/core/registry/FoolishAsteroidsItems.java
+++ src/main/java/com/davigj/foolish_asteroids/core/registry/FoolishAsteroidsItems.java
@@ -1,9 +1,12 @@
 package com.davigj.foolish_asteroids.core.registry;
 
+import com.davigj.foolish_asteroids.common.item.DummyElixirItem;
 import com.davigj.foolish_asteroids.core.FoolishAsteroidsMod;
 import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PotionItem;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -11,5 +14,11 @@ import net.minecraftforge.registries.RegistryObject;
 public class FoolishAsteroidsItems {
 	public static final ItemSubRegistryHelper HELPER = FoolishAsteroidsMod.REGISTRY_HELPER.getItemSubHelper();
 
-	public static final RegistryObject<Item> TEMPLATE_ITEM = HELPER.createItem("template_item", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+	public static final RegistryObject<Item> FLASK = HELPER.createItem("flask", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_BREWING)));
+	public static final RegistryObject<Item> DUMMY_ELIXIR = HELPER.createItem("dummy_elixir", () -> new DummyElixirItem(
+			new Item.Properties().tab(CreativeModeTab.TAB_BREWING).craftRemainder(FoolishAsteroidsItems.FLASK.get()).food(Elixirs.DUMMY_ELIXIR)));
+
+	static class Elixirs {
+		public static final FoodProperties DUMMY_ELIXIR = (new FoodProperties.Builder()).nutrition(0).saturationMod(0).build();
+	}
 }
