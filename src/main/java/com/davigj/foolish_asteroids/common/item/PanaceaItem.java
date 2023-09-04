cee8a754551bbfa99a2545e232c5d4fed4e16bb3
DavigJ <david.j.jung@gmail.com>
2023-09-03 21:44:31 -0400

    elixir relief for panacea

--- src/main/java/com/davigj/foolish_asteroids/common/item/PanaceaItem.java
+++ src/main/java/com/davigj/foolish_asteroids/common/item/PanaceaItem.java
@@ -1,8 +1,11 @@
 package com.davigj.foolish_asteroids.common.item;
 
 import com.davigj.foolish_asteroids.common.util.MorphUtilHelper;
+import com.davigj.foolish_asteroids.core.FoolishAsteroidsMod;
+import com.teamabnormals.blueprint.common.world.storage.tracking.TrackedDataManager;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -15,6 +18,10 @@ import virtuoel.pehkui.api.ScaleTypes;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.davigj.foolish_asteroids.common.item.elixir.HearsayElixirItem.oracleMap;
+import static com.davigj.foolish_asteroids.common.item.elixir.IncendiaryElixirItem.smokingPlayers;
+import static com.davigj.foolish_asteroids.common.item.elixir.IndomitableElixirItem.rainbowTimers;
+
 public class PanaceaItem extends Item {
 
     public PanaceaItem(Properties properties) {
@@ -22,15 +29,30 @@ public class PanaceaItem extends Item {
     }
     public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity consumer) {
         if (!level.isClientSide) {
-
-            this.affectConsumer(stack, level, consumer);
+            this.affectConsumer(consumer);
         }
         return this.isEdible() ? consumer.eat(level, stack) : stack;
     }
 
-    public void affectConsumer(ItemStack stack, Level level, LivingEntity entityLiving) {
+    public void affectConsumer(LivingEntity entityLiving) {
         if (entityLiving instanceof  Player player) {
+            TrackedDataManager manager = TrackedDataManager.INSTANCE;
             String entityTag = player.getDisplayName().getString();
+            smokingPlayers.remove(player.getUUID());
+            rainbowTimers.remove(player.getUUID());
+            oracleMap.remove(player.getUUID());
+            if (manager.getValue(player, FoolishAsteroidsMod.HIGHWAY_TO_HELL) != 0) {
+                manager.setValue(player, FoolishAsteroidsMod.HIGHWAY_TO_HELL, 0);
+                TranslatableComponent message = new TranslatableComponent("message.hearsay.forgiveness");
+                player.displayClientMessage(message, true);
+            } else if (manager.getValue(player, FoolishAsteroidsMod.SERAPHIC_ACTIVE)) {
+                manager.setValue(player, FoolishAsteroidsMod.SERAPHIC_ACTIVE, false);
+            }
+            manager.setValue(player, FoolishAsteroidsMod.STORED_ELECTRONS, 0);
+            manager.setValue(player, FoolishAsteroidsMod.RAD_POISONING, false);
+            manager.setValue(player, FoolishAsteroidsMod.AUTUMNAL, false);
+            manager.setValue(player, FoolishAsteroidsMod.ANTI_DRUNK, 0);
+
             if (MorphUtilHelper.isPlayerMorphed(player)) {
                 String commandToExecute = "/demorph " + entityTag;
                 CommandSourceStack commandSource = player.createCommandSourceStack();
